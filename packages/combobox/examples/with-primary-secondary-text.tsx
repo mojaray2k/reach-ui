a1c30c2f2306477e0460d57071f62ccfc8ecad33
Amen Moja Ra <mojaray2k@gmail.com>
2021-12-27 17:13:04 +0100

    bg-fixes branch

--- packages/combobox/examples/with-primary-secondary-text.tsx
+++ packages/combobox/examples/with-primary-secondary-text.tsx
@@ -9,68 +9,87 @@ import {
 import { useCityMatch } from "./utils";
 import "@reach/combobox/styles.css";
 import { ComboboxObjectValue } from "../src";
-import { checkTypeOfInput } from "../src/utils";
-import { values } from "lodash";
-const bookOfMormonHeroes = [
-  { primaryText: "Lehi" },
-  { primaryText: "Nephi" },
-  { primaryText: "Jacob", secondaryText: "Brother of Lehi" },
-  { primaryText: "Enos", secondaryText: "Son of Jacob" },
-  { primaryText: "King Benjamin", secondaryText: "Father of Mosiah" },
-  { primaryText: "Abinadi", secondaryText: "Testified before King Noah" },
+
+const suggestions = [
   {
-    primaryText: "Alma the Elder",
-    secondaryText: "Believed the words of Abinidi",
-  },
-  { primaryText: "Alma the Younger", secondaryText: "Visited by an Angel" },
-  {
-    primaryText: "Ammon",
-    secondaryText: "Converted the king and queen and all his servants",
+    key: "1",
+    superPowerValue: 5,
+    primaryText: "Iron Man",
+    secondaryText: "Tony Stark",
   },
   {
-    primaryText: "Captain Moroni",
-    secondaryText: "Great Nephite Warrior",
-    tertiaryText: "Title of Liberty",
+    key: "2",
+    superPowerValue: 10,
+    primaryText: "Hulk",
+    secondaryText: "Bruce Banner",
   },
   {
-    primaryText: "Two Thousand Stripling Warriors",
-    secondaryText: "Believed the words of their mothers",
-    tertiaryText: "all were wounded but none perished",
-  },
-  {
-    primaryText: "Samuel the lamanite",
-    secondaryText: "Prophesied of Christs Birth",
-  },
-  {
-    primaryText: "Mormon",
-    secondaryText: "Abridged the Plates from many books",
-  },
-  {
-    primaryText: "Moroni",
-    secondaryText: "Son of Mormon",
-    tertiaryText: "Added the plates of Ether and his own testimony.",
-  },
-  {
-    primaryText: "The Brother of Jared",
-    secondaryText: "Saw the Finger of God",
-    tertiaryText:
-      "Came to the promised land by barges that were tight like unto a dish.",
-  },
-  {
-    primaryText: "Samuel The Lamanite",
-    secondaryText: "Prophesied to the Nephites",
-  },
-  {
-    primaryText: "Abish",
-    secondaryText: "Shared King and Queen's conversion with entire village.",
+    key: "3",
+    superPowerValue: 10,
+    primaryText: "Thor",
+    secondaryText: "Thor Odinson",
   },
 ];
+export function removeDiacritics(str: any) {
+  // normalize() converts a string into unicode, from which we can replace a range of diacritic characters
+  // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+const controlsSuggestions = suggestions.slice(0, 3).map((suggestion) => {
+  return {
+    key: suggestion.key,
+    primaryText: suggestion.primaryText,
+    secondaryText: suggestion.secondaryText,
+    superPowerValue: suggestion.superPowerValue,
+  };
+});
 let name = "With Primary and Secondary Text (TS)";
+function createInputSuggestion(input: any) {
+  return {
+    key: "INPUT_TEXT_OPTION",
+    primaryText: input.trim(),
+    type: "INPUT_TEXT_OPTION",
+  };
+}
 
-function Example() {
+function shouldKeepInputValue(state: any, changes: any) {
+  // Autosuggest design is to keep the current input value when LAST_RESORT is selected.
+  return changes.selectedItem && changes.selectedItem.type === "LAST_RESORT";
+}
+
+function includesInputText(
+  suggestionText: any,
+  inputText: any,
+  ignoreDiacritics: any
+) {
+  if (suggestionText) {
+    const normalizedSuggestionText = (
+      ignoreDiacritics ? removeDiacritics(suggestionText) : suggestionText
+    ).toLowerCase();
+    const normalizedInputText = (
+      ignoreDiacritics ? removeDiacritics(inputText) : inputText
+    )
+      .toLowerCase()
+      .trim();
+    return normalizedSuggestionText.includes(normalizedInputText);
+  }
+  return false;
+}
+function Example({
+  noFilter = false,
+  handleInput = false,
+  ignoreDiacritics = true,
+  chromeAutoCompleteOff = "off",
+  clearOnClickAway = false,
+  showLastResortOnFocus = false,
+  maxLength = 512,
+  keepUserInputTextAlways = false,
+  virtualize = false,
+  sortFunction = null,
+  lastResort,
+}) {
   let [term, setTerm] = React.useState("");
-  let [selection, setSelection] = React.useState<any>("");
-  let bookOfMormonHeroes = useCityMatch(term);
   let ref = React.useRef(null);
 
   const handleChange = (event: any) => {
@@ -78,8 +97,38 @@ function Example() {
   };
 
   const handleSelect = (value: ComboboxObjectValue | string) => {
-    setSelection(value);
+    // setSelection(value);
+    console.log("Hello", value);
   };
+  let displaySuggestions = [...controlsSuggestions];
+  if (!noFilter && term) {
+    displaySuggestions = displaySuggestions.filter(
+      (suggestion) =>
+        includesInputText(suggestion.primaryText, term, ignoreDiacritics) ||
+        includesInputText(suggestion.secondaryText, term, ignoreDiacritics)
+      // includesInputText(suggestion.tertiaryText, term, ignoreDiacritics)
+    );
+  }
+  // User-provided sort function for suggestions
+  if (sortFunction && typeof sortFunction === "function") {
+    displaySuggestions.sort((sortFunction as any).bind(null, term));
+  }
+  if (lastResort && (term || showLastResortOnFocus)) {
+    displaySuggestions.push({
+      ...lastResort,
+      key: "LAST_RESORT",
+      type: "LAST_RESORT",
+    });
+  }
+  if (
+    handleInput &&
+    term &&
+    !displaySuggestions.find(
+      (s) => s.primaryText.toLowerCase() === term.toLowerCase().trim()
+    )
+  ) {
+    // displaySuggestions.unshift(createInputSuggestion(term));
+  }
 
   return (
     <div>
@@ -93,9 +142,9 @@ function Example() {
           autocomplete={true}
           style={{ width: 400 }}
         />
-        {bookOfMormonHeroes && (
+        {displaySuggestions && (
           <ComboboxPopover>
-            {bookOfMormonHeroes.length === 0 && (
+            {displaySuggestions.length === 0 && (
               <p>
                 No Results{" "}
                 <button
@@ -110,14 +159,14 @@ function Example() {
               </p>
             )}
             <ComboboxList>
-              {bookOfMormonHeroes.slice(0, 10).map((result, index) => {
+              {displaySuggestions.slice(0, 10).map((result, index) => {
                 return (
                   <ComboboxOption
                     key={index}
                     value={{
                       key: index,
-                      city: `${result.city}`,
-                      state: `${result.state}`,
+                      primaryText: `${result.primaryText}`,
+                      secondaryText: `${result.secondaryText}`,
                     }}
                   />
                 );
